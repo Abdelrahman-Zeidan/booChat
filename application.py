f7cb623dc8ea38017387b0bdcfaf9ffca102cd54
Abdelrahman-Zeidan <48242953+Abdelrahman-Zeidan@users.noreply.github.com>
2019-12-01 18:21:37 +0200

    Add flask_wtf for registration page

--- application.py
+++ application.py
@@ -3,15 +3,27 @@ import os
 from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
 
+
+from wtform_fields import *
+
+
+#Configure app
 app = Flask(__name__)
+
 app.secret_key = 'replace later'
-app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
+
+SECRET_KEY = os.urandom(32)
+app.config['SECRET_KEY'] = SECRET_KEY
 socketio = SocketIO(app)
 
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-    return render_template("index.html")
+    reg_form = RegistrationForm()
+    if reg_form.validate_on_submit():
+        return "Greate success!"
+
+    return render_template("index.html", form=reg_form)
 
 
 
