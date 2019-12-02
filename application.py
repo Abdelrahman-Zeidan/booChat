0369bf6e47b63ec2ea6d61b943bfb4fbf2c475de
Abdelrahman-Zeidan <48242953+Abdelrahman-Zeidan@users.noreply.github.com>
2019-12-02 11:30:45 +0200

    Update registration form duplicate username checker

--- application.py
+++ application.py
@@ -21,6 +21,7 @@ app.config['SECRET_KEY'] = SECRET_KEY
 app.config['SQLALCHEMY_DATABASE_URI']='postgres://ywfswkkvelnjjo:c0e02d3ca9a527673dee4e990ec814023ae9b932c0d0b0d65c2277bf7e802a3d@ec2-23-21-248-1.compute-1.amazonaws.com:5432/ddn24ofdllas71'
 db = SQLAlchemy(app)
 
+
 socketio = SocketIO(app)
 
 
@@ -31,11 +32,6 @@ def index():
     if reg_form.validate_on_submit():
         username = reg_form.username.data
         password = reg_form.password.data
-
-        # Check username exists
-        user_object = User.query.filter_by(username=username).first()
-        if user_object:
-            return "Sorry this name already exits, Try another name"
         
         # Add user to the database
         user = User(username=username, password=password)
