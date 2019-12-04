f32145b928750b65cbfa91831d4ca3551c8ab878
Abdelrahman-Zeidan <48242953+Abdelrahman-Zeidan@users.noreply.github.com>
2019-12-04 15:29:09 +0200

    Add message flashing

--- application.py
+++ application.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, flash
 from flask_login import LoginManager, login_user, current_user, login_required, logout_user
 
 from flask_socketio import SocketIO, emit
@@ -60,6 +60,8 @@ def index():
         db.session.add(user)
         db.session.commit()
 
+        flash('Registered successfully, Please login.', 'success')
+
         return redirect(url_for('login'))
 
     return render_template("index.html", form=reg_form)
@@ -95,7 +97,8 @@ def login():
 def chat():
 
     if not current_user.is_authenticated:
-        return "Please login before accessing chat"
+        flash('Please login.', 'danger')
+        return redirect(url_for('login'))
 
     return "Chat with me"
 
@@ -115,7 +118,8 @@ def chat():
 def logout():
 
     logout_user()
-    return "logged out using flask-login!"
+    flash('You have logged out successfully', 'success')
+    return redirect(url_for('login'))
 
 
 
